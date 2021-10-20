5641886c730696efedd15692321f8177997e4382
Nusrath Tanha <muskantanha87@gmail.com>
2021-10-20 18:54:44 +0600

    finally

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import useAuth from '../../hooks/useAuth';
 import '../Login/login.css'
+import login from '../../images/login-img.png'
+import { Button } from 'react-bootstrap';
 
 
 
@@ -9,17 +11,22 @@ const Login = () => {
     const{signInWithGoogle,handleEvent,emailVery,passWord,error,toogleSing,isLogging,registerNewUser}=useAuth()
  
     return (
-        <div className="col-md-12 login">
+        <div className="login-page d-flex p-5">
+            <div className="col-md-6">
+                <img className="w-50 fluid" src={login} alt=""/>
+
+            </div>
+        <div className="col-md-6 ">
             <form onSubmit={handleEvent}>
-            <h2>Please{isLogging?'Login':'Register'}</h2>
-            <input className="my-3 w-25" onChange={emailVery} type="email" name="" placeholder="Enter your email" id="" required />
+            <h2 className="service-title">Please  {isLogging?'Login':'Register'}</h2>
+            <input className="my-3 w-75" onChange={emailVery} type="email" name="" placeholder="Enter your email" id="" required />
             <br/>
-            <input className="w-25" onChange={passWord} type="password" name="" placeholder="Enter your password" id="" required />
+            <input className="w-75" onChange={passWord} type="password" name="" placeholder="Enter your password" id="" required />
 
             <br/>
             <br/>
             
-            <button className="px-5" onChange={registerNewUser}>Enter</button>
+            <button className="px-5 py-1 my-1 rounded-3" onChange={registerNewUser}>Enter</button>
             <br/>
             <input onChange={toogleSing} type="checkbox" name="" id="" />
             
@@ -28,8 +35,10 @@ const Login = () => {
             <br/>
             <br/>
             <br/>
-            <button onClick={signInWithGoogle}>Google sign-in</button>
+            <Button variant="secondary" className="px-5" onClick={signInWithGoogle}><i className="fab fa-google"></i>  Google sign-in</Button>
+            
             </form>
+            </div>
             
             
         </div>
